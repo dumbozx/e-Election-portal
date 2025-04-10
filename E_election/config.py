c227a619d1810da965198d752756eaaeff6d612b
Nilesh <friendisrude@gmail.com>
2025-04-10 20:53:27 +0530

    Delete E_election directory

--- E_election/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-very-secret-key-change-this' # IMPORTANT: Use environment variable
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///election.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
